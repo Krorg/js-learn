fdc9b7dba1051e9df60a988fb24a08422ecc25ae
Krorg <krorgdev@gmail.com>
2024-05-23 17:58:26 +0300

    add redux store, Counter component (first entity)

---
+++ src/entities/Counter/index.ts
@@ -0,0 +1,5 @@
+import { counetrReducer } from './model/slice/counterSlice';
+import { Counter } from './ui/Counter';
+import type { CounterSchema } from './model/types/counterSchema';
+
+export { counetrReducer, Counter, CounterSchema };
---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,26 @@
+import { counetrReducer, counterActions } from './counterSlice';
+import { CounterSchema } from '../types/counterSchema';
+
+describe('counterSlice.test', () => {
+    test('increment', () => {
+        const state: CounterSchema = {
+            value: 10,
+        };
+        expect(counetrReducer(state, counterActions.increment)).toEqual({
+            value: 11,
+        });
+    });
+    test('decrement', () => {
+        const state: CounterSchema = {
+            value: 10,
+        };
+        expect(counetrReducer(state, counterActions.decrement)).toEqual({
+            value: 9,
+        });
+    });
+    test('should work with empty state', () => {
+        expect(counetrReducer(undefined, counterActions.increment)).toEqual({
+            value: 1,
+        });
+    });
+});
