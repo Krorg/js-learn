03a00c866456329989c8a4491ea9f2647323010f
Krorg <krorgdev@gmail.com>
2024-05-27 10:33:54 +0300

    add new input, add login form ui

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,12 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { ReducersMapObject, configureStore } from '@reduxjs/toolkit';
 import { counetrReducer } from 'entities/Counter';
+import { userReducer } from 'entities/User';
 import { StateSchema } from './StateSchema';
 
 export function createReduxStore(initialState?: StateSchema) {
+    const rootReducers: ReducersMapObject<StateSchema> = {
+        counter: counetrReducer,
+        user: userReducer,
+    };
+
     return configureStore<StateSchema>({
-        reducer: {
-            counter: counetrReducer,
-        },
+        reducer: rootReducers,
         devTools: __IS_DEV__,
         preloadedState: initialState,
     });
