2b937b75268040b39781bd848172ea1acc52aa8f
Krorg <krorgdev@gmail.com>
2024-07-11 17:58:24 +0300

    prepare for prod

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -2,10 +2,4 @@ import { lazy } from 'react';
 
 // export const AboutPageAsync = lazy(() => import('./AboutPage'));
 
-export const AboutPageAsync = lazy(
-    () =>
-        new Promise((resolve) => {
-            // @ts-ignore
-            setTimeout(() => resolve(import('./AboutPage')), 1500);
-        })
-);
+export const AboutPageAsync = lazy(() => import('./AboutPage'));
--- src/pages/ProfilePage/ui/ProfilePage.async.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.async.tsx
@@ -1,10 +1,3 @@
 import { lazy } from 'react';
 
-export const ProfilePageAsync = lazy(
-    () =>
-        new Promise((resolve) => {
-            // @ts-ignore
-            // Задержка для тест, потом удалить
-            setTimeout(() => resolve(import('./ProfilePage')), 1500);
-        })
-);
+export const ProfilePageAsync = lazy(() => import('./ProfilePage'));
