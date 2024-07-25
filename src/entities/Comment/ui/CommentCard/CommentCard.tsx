8b634846de3ae99de2fcc75245ff7ee1360cd54c
Krorg <krorgdev@gmail.com>
2024-07-25 10:08:38 +0300

    fix styles

--- src/entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -21,7 +21,9 @@ export const CommentCard = memo((props: CommentCardProps) => {
 
     if (isLoading) {
         return (
-            <div
+            <VStack
+                gap="16"
+                max
                 className={classNames(cls.CommentCard, {}, [
                     className,
                     cls.loading,
@@ -36,7 +38,7 @@ export const CommentCard = memo((props: CommentCardProps) => {
                     />
                 </div>
                 <Skeleton className={cls.text} width="100%" height={50} />
-            </div>
+            </VStack>
         );
     }
     if (!comment) {
