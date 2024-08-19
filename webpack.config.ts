54239005447cbe31a6ed15120e7590b1208d9c76
Krorg <krorgdev@gmail.com>
2024-08-19 13:59:53 +0300

    add e2e component test case

--- webpack.config.ts
+++ webpack.config.ts
@@ -13,9 +13,9 @@ export default (env: BuildEnv) => {
         buildLocales: path.resolve(__dirname, 'build', 'locales'),
     };
 
-    const mode = env.mode || 'development';
-    const PORT = env.port || 3000;
-    const apiUrl = env.apiUrl || 'http://192.168.10.124:8000';
+    const mode = env?.mode || 'development';
+    const PORT = env?.port || 3000;
+    const apiUrl = env?.apiUrl || 'http://192.168.10.124:8000';
 
     const isDev = mode === 'development';
 
